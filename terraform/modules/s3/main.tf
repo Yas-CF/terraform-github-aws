868e18550a2edf39a96ab5c082e5d1d777bd7b89
Yas-CF <yasna.kcf@gmail.com>
2025-01-03 22:43:05 -0300

    Update main.tf

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -12,12 +12,6 @@ resource "aws_s3_bucket" "bucket" {
   }
 }
 
-resource "aws_s3_object" "lambda_code" {
-  bucket = aws_s3_bucket.bucket.bucket
-  key    = var.file_key
-  source = var.file_path
-}
-
 resource "aws_s3_bucket_versioning" "versioning" {
   bucket = aws_s3_bucket.bucket.id
   versioning_configuration {
@@ -32,4 +26,49 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
       sse_algorithm = "AES256"
     }
   }
+}
+
+resource "aws_iam_role" "s3_backend_role" {
+  name               = "TerraformS3BackendRole-${random_id.id.hex}"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "s3_backend_policy" {
+  name        = "TerraformS3BackendPolicy-${random_id.id.hex}"
+  description = "Policy granting access to S3 for Terraform backend"
+  policy      = data.aws_iam_policy_document.s3_backend_policy.json
+}
+
+data "aws_iam_policy_document" "s3_backend_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:ListBucket",
+      "s3:GetBucketLocation"
+    ]
+    resources = [
+      aws_s3_bucket.bucket.arn,
+      "${aws_s3_bucket.bucket.arn}/*"
+    ]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "s3_backend_policy_attachment" {
+  role       = aws_iam_role.s3_backend_role.name
+  policy_arn = aws_iam_policy.s3_backend_policy.arn
 }
