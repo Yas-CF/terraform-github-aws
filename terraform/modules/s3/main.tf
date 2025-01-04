c96657f7078abcafbcb9aca4f1595b1545b96068
Yas-CF <yasna.kcf@gmail.com>
2025-01-03 23:54:12 -0300

    Update main.tf

--- terraform/modules/s3/main.tf
+++ terraform/modules/s3/main.tf
@@ -68,18 +68,3 @@ resource "aws_iam_role_policy_attachment" "s3_backend_policy_attachment" {
   role       = aws_iam_role.s3_backend_role.name
   policy_arn = aws_iam_policy.s3_backend_policy.arn
 }
-
-output "bucket_name" {
-  value       = aws_s3_bucket.bucket.id
-  description = "The name of the S3 bucket."
-}
-
-output "bucket_arn" {
-  value       = aws_s3_bucket.bucket.arn
-  description = "The ARN of the S3 bucket."
-}
-
-output "s3_backend_role_arn" {
-  value       = aws_iam_role.s3_backend_role.arn
-  description = "The ARN of the IAM role for S3 backend."
-}
