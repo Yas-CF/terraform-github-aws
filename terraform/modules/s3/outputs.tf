0fa6a496ff605c7961861824d801627be5f492c2
Yas-CF <yasna.kcf@gmail.com>
2025-01-03 23:48:45 -0300

    Update outputs.tf

--- terraform/modules/s3/outputs.tf
+++ terraform/modules/s3/outputs.tf
@@ -7,12 +7,10 @@ output "backend_config" {
   description = "Configuration for Terraform S3 backend."
 }
 
-# Outputs
 output "bucket_name" {
-  value       = aws_s3_bucket.bucket.id
-  description = "The name of the S3 bucket created."
+  value = aws_s3_bucket.bucket.id
+  description = "The name of the S3 bucket."
 }
-
 output "bucket_arn" {
   value       = aws_s3_bucket.bucket.arn
   description = "The ARN of the S3 bucket created."
