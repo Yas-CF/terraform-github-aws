171d30aa8883c937505a96d301e8d47cbd8677ee
Yas-CF <yasna.kcf@gmail.com>
2025-01-03 23:04:50 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = us-east-1
+  region  = "us-east-1"
 }
 
 module "s3" {
@@ -8,7 +8,7 @@ module "s3" {
   file_key    = var.file_key
   file_path   = var.file_path
   environment = var.environment
-  aws_region  = us-east-1
+  aws_region  = "us-east-1"
 }
 
 terraform {
